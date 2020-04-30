cf32df6eb873ffbbaf40954396f724583a702f86
Lnola <nola.luka@gmail.com>
2020-04-30 04:02:05 +0200

    Deploy

---
+++ galerijaapp/build/precache-manifest.85aec112b4162ab4df60d9eb3ee2d1a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0edbabba94a7c1d17ec2b45cf72a0ab",
+    "url": "/Galerija/index.html"
+  },
+  {
+    "revision": "c6c7b7dd19d9ce98139f",
+    "url": "/Galerija/static/css/main.c1c01b48.chunk.css"
+  },
+  {
+    "revision": "225e5fe2631fbdce3009",
+    "url": "/Galerija/static/js/2.a6086c47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Galerija/static/js/2.a6086c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c7b7dd19d9ce98139f",
+    "url": "/Galerija/static/js/main.f79b3ed1.chunk.js"
+  },
+  {
+    "revision": "a335c5994707c2ce4111",
+    "url": "/Galerija/static/js/runtime-main.e91e8367.js"
+  },
+  {
+    "revision": "d6d52a98f25d5c3045141c60768caf7b",
+    "url": "/Galerija/static/media/peristil.d6d52a98.jpg"
+  }
+]);
