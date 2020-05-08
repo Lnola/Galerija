a1d3126481c8ccd1e24b8ea8c9c9525c9b72302a
Lnola <nola.luka@gmail.com>
2020-05-08 02:46:40 +0200

    Finished Sun screen

--- galerijaapp/src/App.js
+++ galerijaapp/src/App.js
@@ -42,8 +42,8 @@ const App = () => {
       {/* <Expressionism /> */}
       {/* <Bird /> */}
       {/* <Artist /> */}
-      <SamIV />
-      {/* <Sun /> */}
+      {/* <SamIV /> */}
+      <Sun />
       {/* <Dancing /> */}
       {/* <Garbage /> */}
     </StyledMain>
--- galerijaapp/src/screens/Sun/index.js
+++ galerijaapp/src/screens/Sun/index.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import { FlexSection } from "../../components/styled/Flex";
+import SunImage from "../../images/sun.png";
+
+const StyledImage = styled.img`
+  height: 90%;
+`;
+
+const Textarea = styled.textarea`
+  position: absolute;
+  bottom: 15%;
+  left: 47.8%;
+  width: 46.8%;
+  height: 25.7%;
+  resize: none;
+  font-size: 22px;
+  border-color: transparent;
+  background-color: transparent;
+  padding: 3px 5px;
+  box-sizing: border-box;
+`;
 
 const Sun = () => {
-  return <></>;
+  const [input, setInput] = useState("");
+
+  return (
+    <FlexSection
+      height="100%"
+      justifyContent="center"
+      alignItems="center"
+      position="relative"
+    >
+      <StyledImage
+        src={SunImage}
+        alt="U ime ja"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <Textarea />
+    </FlexSection>
+  );
 };
 
 export default Sun;
